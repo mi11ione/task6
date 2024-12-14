f839d87484bb6446aa4e05258ff6f7c426c7a4ca
mi11ione <i@mi11ion.io>
2024-12-14 10:44:55 +0300

    Initial Commit

---
+++ task6/task6App.swift
@@ -0,0 +1,17 @@
+//
+//  task6App.swift
+//  task6
+//
+//  Created by mi11ion on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct task6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
